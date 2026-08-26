f7b00762781336265d1a06f8a30068a9d5c04ad5
Judook <devdk2008@gmail.com>
2026-08-26 15:49:00 +0530

    a random commit

--- walls.cpp
+++ walls.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath> 
+#include <array>
 using namespace std;
 
 struct Vec2 {
@@ -28,6 +29,17 @@ int march(Vec2 pos, float angle, char world[20][30]) {
     return marchVector;
 }
 
+array<int, 100> scan (float playerAngle, float fov, Vec2 pos, char world[20][30]) {
+    float piece = fov/100;
+    int i;
+    array<int, 100> out = {};
+    float scanAngle;
+    for(scanAngle = playerAngle - fov/2, i=0; scanAngle <= playerAngle + fov/2; scanAngle += piece, i++) {
+        out[i] = march(pos, scanAngle, world);
+    }
+    return out;
+}
+
 int main() {
     char world[20][30] = {
         ".............................", 
@@ -54,7 +66,9 @@ int main() {
     Vec2 playerPos = {10.0f, 20.0f};
     float playerAngle = 0.0f;
     float playerFOV = 60.0f;
-    cout<<"Hello";
     
+    array<int, 100> a = scan(180.0f, 90.0f, playerPos, world);
+    for(int i=0; i<100; i++) cout<<a[i]<<" ";
+
     return 0;
 }
